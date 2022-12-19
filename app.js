480adf78f699d9ccce7c0e498b46c79af0062728
eNeville <17412822+eNeville@users.noreply.github.com>
2022-12-19 16:21:31 -0400

    Add openMenuHandler, closeMenuHandler

---
+++ app.js
@@ -0,0 +1,18 @@
+const menuIcon = document.getElementById('menu-bars');
+const mobileMenu = document.getElementById('mobileMenu');
+
+function openMenuHandler() {
+  console.log('Hello');
+  mobileMenu.classList.replace('d-none', 'd-flex');
+}
+
+menuIcon.addEventListener('click', openMenuHandler);
+
+const closeMenu = document.getElementById('closeMenu');
+
+function closeMenuHandler() {
+  console.log('Hello');
+  mobileMenu.classList.replace('d-flex', 'd-none');
+}
+
+closeMenu.addEventListener('click', closeMenuHandler);
