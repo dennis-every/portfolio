4b0ceac422d68a653316eafc8a138b92629b556c
Hans-Derly Rameau <rameauhansderly@gmail.com>
2022-12-19 16:53:08 -0400

    close the mobile menu when the user clicks (or taps) any of the mobile menu options

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const menuIcon = document.getElementById('menu-bars');
 const mobileMenu = document.getElementById('mobileMenu');
 
 function openMenuHandler() {
-  console.log('Hello');
-  mobileMenu.classList.replace('d-none', 'd-flex');
+	console.log('Hello');
+	mobileMenu.classList.replace('d-none', 'd-flex');
 }
 
 menuIcon.addEventListener('click', openMenuHandler);
@@ -11,8 +11,11 @@ menuIcon.addEventListener('click', openMenuHandler);
 const closeMenu = document.getElementById('closeMenu');
 
 function closeMenuHandler() {
-  console.log('Hello');
-  mobileMenu.classList.replace('d-flex', 'd-none');
+	console.log('Hello');
+	mobileMenu.classList.replace('d-flex', 'd-none');
 }
 
 closeMenu.addEventListener('click', closeMenuHandler);
+
+const mobileLinks = document.getElementById('mobile-links');
+mobileLinks.addEventListener('click', closeMenuHandler);
