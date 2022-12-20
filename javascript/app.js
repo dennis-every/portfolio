68789a62ed165b7ed214f6a4f2f5fe50dc67597e
eNeville <17412822+eNeville@users.noreply.github.com>
2022-12-20 17:25:21 -0400

    Remove static works html, add js to generate works cards on window.load

--- javascript/app.js
+++ javascript/app.js
@@ -30,7 +30,8 @@ const worksArray = [
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     technologies: ['html', 'css', 'javascript'],
-    url: 'images/work_1.jpg',
+    urlMobile: '/images/work_1.jpg',
+    urlDesktop: '/images/work_1_lg.png',
     alt: 'work_tonic_daily_personalized_reads',
     live: 'https://dennis-every.github.io/',
     source: 'https://github.com/dennis-every/portfolio',
@@ -41,7 +42,8 @@ const worksArray = [
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     technologies: ['html', 'css', 'javascript'],
-    url: 'images/work_2.jpg',
+    urlMobile: 'images/work_2.jpg',
+    urlDesktop: 'images/work_2_lg.png',
     alt: 'work_multi-post_stories_daily_personalized_reads',
     live: 'https://dennis-every.github.io/',
     source: 'https://github.com/dennis-every/portfolio',
@@ -52,7 +54,8 @@ const worksArray = [
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     technologies: ['html', 'css', 'javascript'],
-    url: 'images/work_3.jpg',
+    urlMobile: 'images/work_3.jpg',
+    urlDesktop: 'images/work_3_lg.png',
     alt: 'work_tonic_daily_personalized_reads',
     live: 'https://dennis-every.github.io/',
     source: 'https://github.com/dennis-every/portfolio',
@@ -63,10 +66,62 @@ const worksArray = [
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     technologies: ['html', 'css', 'javascript'],
-    url: 'images/work_4.jpg',
+    urlMobile: 'images/work_4.jpg',
+    urlDesktop: 'images/work_4_lg.png',
     alt: 'work_multi-post_stories_daily_personalized_reads',
     live: 'https://dennis-every.github.io/',
     source: 'https://github.com/dennis-every/portfolio',
   },
 ];
-console.log(worksArray);
+
+const worksPopupHandler = () => {
+  const works = document.getElementById('works');
+  worksArray.forEach((work) => {
+    const workCard = document.createElement('li');
+    workCard.classList.add('work-card');
+    workCard.innerHTML = `
+      <article class='work-card__article'>
+        <div class='work-card__header'>
+          <picture>
+            <source srcset=${work.urlDesktop} media='(min-width: 768px)' />
+            <img
+              src=${work.urlMobile}
+              alt=${work.alt}
+            />
+          </picture>
+        </div>
+        <div class='work-card__body'>
+          <div class='elements'>
+            <header class='work-card__title'>${work.title}</header>
+            <div>
+              <ul class='work-card__experience'>
+                <li>${work.experiences[0]}</li>
+                <li>&#x2022;</li>
+                <li>${work.experiences[1]}</li>
+                <li>&#x2022;</li>
+                <li>${work.experiences[2]}</li>
+              </ul>
+            </div>
+            <p class='work-card__description'>${work.description}</p>
+            <div>
+              <ul class='work-card__technologies'>
+                <li>${work.technologies[0]}</li>
+                <li>${work.technologies[1]}</li>
+                <li>${work.technologies[2]}</li>
+              </ul>
+            </div>
+            <div class='btn-container'>
+              <button type='button' class='btn'>
+                <a href=${work.live}>See Project</a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </article>`;
+    works.appendChild(workCard);
+  });
+};
+
+window.onload = () => {
+  worksPopupHandler();
+};
