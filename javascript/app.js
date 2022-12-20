ed9dd635a1e71f33856afd46929ec1d76dfc9e41
Hans-Derly Rameau <rameauhansderly@gmail.com>
2022-12-20 16:48:01 -0400

    add js array data

--- javascript/app.js
+++ javascript/app.js
@@ -22,3 +22,51 @@ function closeMenuHandler() {
 menuBars.addEventListener('click', openMenuHandler);
 closeMenuBtn.addEventListener('click', closeMenuHandler);
 mobileLinks.addEventListener('click', closeMenuHandler);
+
+const worksArray = [
+  {
+    title: 'Tonic',
+    experiences: ['Canopy', 'Back End Dev', '2015'],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['html', 'css', 'javascript'],
+    url: 'images/work_1.jpg',
+    alt: 'work_tonic_daily_personalized_reads',
+    live: 'https://dennis-every.github.io/',
+    source: 'https://github.com/dennis-every/portfolio',
+  },
+  {
+    title: 'Multi-Post Stories',
+    experiences: ['Canopy', 'Back End Dev', '2015'],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['html', 'css', 'javascript'],
+    url: 'images/work_2.jpg',
+    alt: 'work_multi-post_stories_daily_personalized_reads',
+    live: 'https://dennis-every.github.io/',
+    source: 'https://github.com/dennis-every/portfolio',
+  },
+  {
+    title: 'Tonic',
+    experiences: ['Canopy', 'Back End Dev', '2015'],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['html', 'css', 'javascript'],
+    url: 'images/work_3.jpg',
+    alt: 'work_tonic_daily_personalized_reads',
+    live: 'https://dennis-every.github.io/',
+    source: 'https://github.com/dennis-every/portfolio',
+  },
+  {
+    title: 'Multi-Post Stories',
+    experiences: ['Canopy', 'Back End Dev', '2015'],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['html', 'css', 'javascript'],
+    url: 'images/work_4.jpg',
+    alt: 'work_multi-post_stories_daily_personalized_reads',
+    live: 'https://dennis-every.github.io/',
+    source: 'https://github.com/dennis-every/portfolio',
+  },
+];
+console.log(worksArray);
