41368fe1e93df6b7095f4da4bd5ac6f10df76889
eNeville <17412822+eNeville@users.noreply.github.com>
2022-12-20 11:59:13 -0400

    Remove scroll on mobile menu

--- javascript/app.js
+++ javascript/app.js
@@ -3,12 +3,20 @@ const mobileMenu = document.getElementById('mobile-menu');
 const closeMenuBtn = document.getElementById('close-menu-btn');
 const mobileLinks = document.getElementById('mobile-links');
 
-function openMenuHandler() {
+const preventDefaultHandler = (e) => {
+  e.preventDefault();
+};
+
+const openMenuHandler = () => {
   mobileMenu.classList.replace('d-none', 'd-flex');
-}
+  document.body.classList.add('stop-scrolling');
+  document.body.addEventListener('touchmove', preventDefaultHandler);
+};
 
 function closeMenuHandler() {
   mobileMenu.classList.replace('d-flex', 'd-none');
+  document.body.classList.remove('stop-scrolling');
+  document.body.removeEventListener('touchmove', preventDefaultHandler);
 }
 
 menuBars.addEventListener('click', openMenuHandler);
