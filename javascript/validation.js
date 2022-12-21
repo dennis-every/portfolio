ed1a9af61f6792f4ee9035ccc8e60bea4c2c4177
eNeville <17412822+eNeville@users.noreply.github.com>
2022-12-21 15:21:09 -0400

    Add validation.js file and describe agree steps for this branch

---
+++ javascript/validation.js
@@ -0,0 +1,7 @@
+// Steps
+// create new validation.js
+// grab form element
+// grab email input element
+//  on submit, preventDefault, perform validaton
+// if valid form.submit
+// else show an error message near submit button informing them the error and the form is not sent
