2501caf299431628dbbffaae462d94d653d12034
Hans-Derly Rameau <rameauhansderly@gmail.com>
2022-12-22 15:22:01 -0400

    add the steps to follow

---
+++ javascript/localStorage.js
@@ -0,0 +1,14 @@
+// get form elements
+// check local storage available
+// - if available : create local storage object
+// - if not: null
+// create a single object for the data
+// listen to change on input fields
+// stringify that data
+// set the object to local
+// on loads of page
+// - if available:
+// - if local storage has data: put data into fiels
+// if not: nothing
+// - if not: null
+//! create a reset button that reset the input fields and delete data in local storage
