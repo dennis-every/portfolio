5fd88b7ef51b52bf43cd5892c0c4343e158814da
Dennis Every <dennis@ansyra.com>
2023-07-09 15:27:39 -0400

    Update nav header

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+/* eslint-disable no-unused-vars */
+const toggleDarkMode = () => {
+  document.body.classList.toggle('dark-mode');
+
+  const toggleButton = document.getElementById('toggleDarkMode');
+
+  if (document.body.classList.contains('dark-mode')) {
+    toggleButton.innerHTML = '<i class="fa-regular fa-sun"></i>';
+  } else {
+    toggleButton.innerHTML = '<i class="fa-solid fa-moon"></i>';
+  }
+};
