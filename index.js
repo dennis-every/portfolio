9bf6c0853ccead58ee124a310b97515d10f60458
Dennis Every <dennis@ansyra.com>
2023-07-10 17:16:53 -0400

    Updated hero section

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 const toggleDarkMode = () => {
   document.body.classList.toggle('dark-mode');
@@ -5,8 +6,10 @@ const toggleDarkMode = () => {
   const toggleButton = document.getElementById('toggleDarkMode');
 
   if (document.body.classList.contains('dark-mode')) {
-    toggleButton.innerHTML = '<i class="fa-regular fa-sun"></i>';
-  } else {
     toggleButton.innerHTML = '<i class="fa-solid fa-moon"></i>';
+  } else {
+    toggleButton.innerHTML = '<i class="fa-regular fa-sun"></i>';
   }
 };
+
+particlesJS.load('particles-js', 'assets/particles.json');
