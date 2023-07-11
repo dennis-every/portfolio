99aaa71594e8489c46e39bf33556fbc284eccbbf
Dennis Every <dennis@ansyra.com>
2023-07-11 17:38:13 -0400

    Add menu modal

--- index.js
+++ index.js
@@ -14,6 +14,35 @@ const toggleDarkMode = () => {
 
 particlesJS.load('particles-js', 'assets/particles.json');
 
+// ================== Menu ==================
+
+const menuIcon = document.getElementById('menu');
+const mobileMenu = document.getElementById('mobile-menu');
+const closeMenuBtn = document.getElementById('close-menu-btn');
+const mobileLinks = document.getElementById('mobile-links');
+
+const preventDefaultHandler = (e) => {
+  e.preventDefault();
+};
+
+const openMenuHandler = () => {
+  mobileMenu.classList.replace('d-none', 'd-flex');
+  document.body.classList.add('stop-scrolling');
+  document.body.addEventListener('touchmove', preventDefaultHandler);
+};
+
+function closeMenuHandler() {
+  mobileMenu.classList.replace('d-flex', 'd-none');
+  document.body.classList.remove('stop-scrolling');
+  document.body.removeEventListener('touchmove', preventDefaultHandler);
+}
+
+menuIcon.addEventListener('click', openMenuHandler);
+closeMenuBtn.addEventListener('click', closeMenuHandler);
+mobileLinks.addEventListener('click', closeMenuHandler);
+
+// ================== Menu ==================
+
 // Make navigaiton links active when scrolling to the corresponding section
 // Get all the navigation links
 const navLinks = document.querySelectorAll('.nav-link');
